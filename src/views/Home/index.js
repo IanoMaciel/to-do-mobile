d357c90e69e7e8decbe44d336a569f645ea7f4c8
Iano Maciel <ianomaciel6385@gmail.com>
2023-02-15 11:43:19 -0400

    dev: structuring initial files

---
+++ src/views/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+
+const Home = () => {
+    return(
+        <View style={styles.container}>
+            <Text> Tela Home</Text>
+        </View>
+    );
+}
+
+export default Home;
