4a3840c2937b050cb434dd408cb9be5bab0ced27
Iano Maciel <ianomaciel6385@gmail.com>
2023-03-25 23:43:48 -0400

    feat: file utils

---
+++ src/utils/typeIcons.js
@@ -0,0 +1,23 @@
+// icons 
+import iconDefault from '../assets/default.png'
+import iconFood from '../assets/food.png'
+import iconFooteball from '../assets/footeball.png'
+import iconJob from '../assets/job.png'
+import iconParty from '../assets/party.png'
+import iconShopping from '../assets/shopping.png'
+import iconSport from '../assets/sport.png'
+import iconStudy from '../assets/study.png'
+import iconTravel from '../assets/travel.png'
+
+export default [
+    null,
+    iconDefault,
+    iconFooteball,
+    iconFood,
+    iconJob,
+    iconParty,
+    iconStudy,
+    iconShopping,
+    iconTravel,
+    iconSport,
+]
